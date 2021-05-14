ce45f676dd6d6f028f4774fd4b48dc39b3e80147
lmcapacho <lmcapacho@gmail.com>
2021-05-14 18:53:25 -0500

    add socket connection

--- engine/src/engine/server.py
+++ engine/src/engine/server.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import socket
+
+
 class Server:
-    def __init__(self):
-        pass
+    def __init__(self, port):
+        self.port = port
+        self.host = '127.0.0.1'
 
     def run(self):
-        return False
+        conn, addr = self.s.accept()
+        with conn:
+            cmd = conn.recv(4, socket.MSG_WAITALL)
+
+        if not cmd:
+            return False
+
+        self.command = int.from_bytes(cmd, 'little')
+        if self.command == 255:
+            return False
+
+        return True
 
     def config(self):
-        pass
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.bind((self.host, self.port))
+        self.s.listen()
