b7a7ca021bb6a70814609b9296084eb91cbe1d96
lmcapacho <lmcapacho@gmail.com>
2021-05-14 15:54:39 -0500

    create common file

---
+++ engine/src/engine/common.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# commands
+LOAD = 0
+RUN = 1
+STEP = 2
+PAUSE = 3
+STOP = 4
+RESET = 5
+BKP = 6
+MEMORY = 7
+GPIO_W = 8
+GPIO_R = 9
+EXIT = 10
+
+# states
+IDLE = 0
+LOAD = 1
+RUN = 2
+PAUSE = 3
