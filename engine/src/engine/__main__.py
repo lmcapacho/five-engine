7e933a49b29c754e8c36cbb4632c1505ca726c6b
lmcapacho <lmcapacho@gmail.com>
2021-05-15 10:00:33 -0500

    update __main__.py

--- engine/src/engine/__main__.py
+++ engine/src/engine/__main__.py
@@ -2,20 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-
-from engine.server import Server
-
-
-def main(port):
-    engine_server = Server(port)
-    engine_server.config()
-
-    while engine_server.run():
-        pass
+from engine.engine import app
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='RISC-V engine emulator')
-    parser.add_argument('port', help='Socket Port', type=int, default=4321)
+    parser.add_argument('port', help='Socket Port', type=int)
     args = parser.parse_args()
-    main(args.port)
+    app(args.port)
