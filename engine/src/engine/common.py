bdec2ec10eafbf84c55663c81d4326ef75dd8163
lmcapacho <lmcapacho@gmail.com>
2021-05-15 10:42:49 -0500

    enum commands and states

--- engine/src/engine/common.py
+++ engine/src/engine/common.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# commands
-LOAD = 0
-RUN = 1
-STEP = 2
-PAUSE = 3
-STOP = 4
-RESET = 5
-BKP = 6
-MEMORY = 7
-GPIO_W = 8
-GPIO_R = 9
-EXIT = 10
+from enum import Enum
 
-# states
-IDLE = 0
-LOAD = 1
-RUN = 2
-PAUSE = 3
+
+class Cmd(Enum):
+    """ Commands """
+    LOAD = 0
+    RUN = 1
+    STEP = 2
+    PAUSE = 3
+    STOP = 4
+    RESET = 5
+    BKP = 6
+    MEMORY = 7
+    GPIO_W = 8
+    GPIO_R = 9
+    EXIT = 255
+
+
+class State(Enum):
+    """ States """
+    IDLE = 0
+    LOAD = 1
+    RUN = 2
+    PAUSE = 3
